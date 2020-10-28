949e2392e5346ea3817eb9736bc6b89750cebd56
Edgar6996 <45860127+Edgar6996@users.noreply.github.com>
2020-10-28 01:26:05 -0300

    agregue los middleware a las rutas

--- routes/api-routes/calendario.routes.php
+++ routes/api-routes/calendario.routes.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\CalendarioController;
 
 # Grupo para las rutas protegidas
-Route::middleware('auth:api')->prefix("calendario")->group(function () {
+Route::prefix("calendario")->group(function () {
     // definir o importar rutas que deben usar autenticacion
     Route::get('{calendarioId}', [CalendarioController::class, 'show']);
     Route::put('{calendarioId}', [CalendarioController::class, 'update']);
  
  });
- 
+
  Route::post('calendario/prueba', [CalendarioController::class, 'prueba']);
--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,19 @@ use Laravel\Passport\Token;
 
 require_once "api-routes/auth.routes.php";
 require_once "api-routes/lector.routes.php"; // Rutas exclusivas para el sistema Lector de huella
-require_once "api-routes/becados.routes.php"; // Rutas becado requeridas
-require_once "api-routes/calendario.routes.php"; // Rutas calendario becado requeridas
-require_once "api-routes/diarios.routes.php";
 
 # Rutas exclusivas para administradores
 require_once "api-routes/admin.routes.php";
 
 Route::middleware('auth:api')->get('/user', [BecadoControllers::class, 'usuarioActual']);
 
-
+Route::middleware('auth:api')->group(function () {
+    // definir o importar rutas que deben usar autenticacion
+    require_once "api-routes/becados.routes.php"; // Rutas becado requeridas
+    require_once "api-routes/calendario.routes.php"; // Rutas calendario becado requeridas
+    require_once "api-routes/diarios.routes.php";
+ });
+ 
 
 
 
