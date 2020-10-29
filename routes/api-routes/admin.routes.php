90cda8cba5eecf05d11df0c2bca5bba8be21241f
Lucas <lucasroa@outlook.com>
2020-10-29 08:53:17 -0300

    Actualicé el metodo para chequear si un becado completó su registro.

--- routes/api-routes/admin.routes.php
+++ routes/api-routes/admin.routes.php
@@ -10,5 +10,21 @@ Route:: middleware('user.admin')->prefix('admin')->group(function () {
 
     Route::get('users',[\App\Http\Controllers\Users\UsersController::class, "index"]);
 
+
     Route::get('logs', [\App\Http\Controllers\AdminController::class,'indexAppLogs']);
+
+
+    Route::get('check-becados', function () {
+        # Cequeamos el estado de todos los becados
+        $lista = \App\Models\Becado::all();
+        $n = 0;
+        foreach ($lista as $becado) {
+            $becado->checkRegistroCompletado();
+            $n++;
+        }
+
+        $res = new \App\Core\Tools\ApiMessage();
+        $res->setMessage("Se han chequeado {$n} becados");
+        return $res->send();
+    });
 });
