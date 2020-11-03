a8d6a7bfee61699e57a3f9f8e9e34a45995fc875
Edgar6996 <45860127+Edgar6996@users.noreply.github.com>
2020-11-03 18:50:02 -0300

    cree la ruta configuraciones y el metodo update

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -15,4 +15,27 @@ class ConfigController extends Controller
 
         return $res->setData($config)->send();
     }
+
+    public function update(Request $request){
+
+        $res = new ApiMessage;
+
+        $validateData = $request->validate([
+            'max_porciones_becado' => 'int|max:3',
+            'max_porciones_quirofano' => 'int|max:100',
+    
+            'max_faltas' => 'int|max:3',
+            'castigo_duracion_dias' => 'int|max:10',
+    
+            'limite_horas_cancelar_reserva' => 'date',
+            'hora_cena'=> 'date',
+            'hora_almuerzo' => 'date'
+        ]);
+
+        $config = AppConfig::getConfig();
+
+        $config->update($validateData);
+
+        return $res->setData($config)->send();
+    }
 }
