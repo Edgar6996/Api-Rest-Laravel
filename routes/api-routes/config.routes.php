1fc7ffee8b8edc528ea017184413c85266563504
Edgar6996 <45860127+Edgar6996@users.noreply.github.com>
2020-11-04 20:13:53 -0300

    modifique el middleware de configuraciones

--- routes/api-routes/config.routes.php
+++ routes/api-routes/config.routes.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\ConfigController;
 
-Route::middleware('user.admin')->group(function(){
+//Route::middleware('user.admin')->group(function(){
     Route::get('/configuraciones', [ConfigController::class, 'show']);
-    Route::put('/configuraciones', [ConfigController::class, 'update']);
-});
+    Route::put('/configuraciones', [ConfigController::class, 'update'])->middleware('user.admin');
+//});
 
