5d2ce4f841eddbaf9f693ad10c56fa1cdcd5c958
cristianjova <cristian.jova149@gmail.com>
2020-11-09 19:35:56 -0300

    Agregado buscar reserva por id de becado

--- routes/api-routes/diarios.routes.php
+++ routes/api-routes/diarios.routes.php
@@ -7,3 +7,5 @@ Route::get('diario-actual/raciones-disponibles', [DiarioController::class, 'most
 Route::get('diario-actual/mi-reserva', [DiarioController::class, 'showReservaActual']);
 Route::put('diario-actual/cargar-menu', [DiarioController::class, 'cargarMenu']);
 Route::delete('diario-actual/reserva/{id_reserva}', [DiarioController::class, 'cancelarReserva']);
+
+Route::get('diario-actual/mi-reserva/{becado_id}', [DiarioController::class, 'showReservaActualByBecadoId'])->middleware('user.lector'); ;
