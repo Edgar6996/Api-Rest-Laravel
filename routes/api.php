7088f8b22454c67b6f73b9065f7fce18814d2e6a
Lucas <lucasroa@outlook.com>
2020-11-13 09:07:35 -0300

    Nada

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Core\Tools\ApiMessage;
+use App\Enums\CategoriasBecados;
 use App\Http\Controllers\BecadoControllers;
+use App\Models\Calendario;
 use App\Models\AppConfig;
 use App\Models\Diario;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
