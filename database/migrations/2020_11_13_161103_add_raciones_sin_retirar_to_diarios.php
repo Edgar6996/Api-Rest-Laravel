c717942c0ece92480010dd55df9006955f6a4484
Edgardo <edgardo951.ef@gmail.com>
2020-11-13 17:52:31 -0300

    agregue el atributo raciones_sin_retirar al modelo Diario

---
+++ database/migrations/2020_11_13_161103_add_raciones_sin_retirar_to_diarios.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRacionesSinRetirarToDiarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('diarios', function (Blueprint $table) {
+            $table->integer('raciones_sin_retirar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('diarios', function (Blueprint $table) {
+            //
+        });
+    }
+}
