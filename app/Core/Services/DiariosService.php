fbc96b45e27056e6bd82cbda28c1ac0def87f0bc
Lucas <lucasroa@outlook.com>
2020-11-13 11:31:43 -0300

    Reseteamos el contador de faltas al momento de suspender a un becado.

--- app/Core/Services/DiariosService.php
+++ app/Core/Services/DiariosService.php
@@ -82,7 +82,9 @@ class DiariosService
     private function suspenderBecado(Becado $becado){
         $dias_castigo = AppConfig::getConfig()->castigo_duracion_dias;
 
-        $suspendido_hasta = now()->addDays($dias_castigo);
+        $suspendido_hasta = now()
+            ->addDays($dias_castigo)
+            ->subMinutes(5);
 
         $becado->suspendido_hasta = $suspendido_hasta;
 
