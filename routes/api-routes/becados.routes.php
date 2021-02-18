af09b99a22a2842dd7d20190a1d7be034c613762
Lucas <lucasroa@outlook.com>
2021-02-18 12:25:01 -0300

    Nueva ruta para exportar lista de becados

--- routes/api-routes/becados.routes.php
+++ routes/api-routes/becados.routes.php
@@ -9,7 +9,15 @@ Route::get('/becados-reserva', [BecadoControllers::class, 'becadosConReserva'])-
 Route::put('/becados/{becado}', [BecadoControllers::class, 'update']);      // Actualizar
 Route::get('/becados/{becado}', [BecadoControllers::class, 'show']);        // Listar becado individual
 
-Route::get('/becados/report',[ BecadoControllers::class, 'exportListadoPDF'])->middleware('user.admin'); // Generar un PDF con el listado
+Route::get('/becados/report',[ BecadoControllers::class, 'exportListadoPDF']) // Generar un PDF con el listado
+->name('report.becados');
+Route::get('/becados/report-link', function () {
+    $link =  URL::temporarySignedRoute('report.becados', now()->addHours(1));
+    $res = new \App\Core\Tools\ApiMessage();
+    return $res->setCode(200)->setData([
+        'link' => $link
+    ])->send();
+})->middleware('user.admin');
 
 # Route::delete('/becados/{becado}', [BecadoControllers::class, 'destroy']);  // ELiminar becado
 
